f5e9700e4fa2621f32662c31ce16df150367382d
Khalil Nazari <khalil.nazari4@gmail.com>
2019-10-05 18:46:29 +0800

    First commit. In Backend, models and routes are created.

---
+++ backend/routes/exercises.js
@@ -0,0 +1,69 @@
+const experss = require('express')
+const router = experss.Router();
+
+// Model 
+let Exercise = require('../models/exercises.model.js');
+
+// Get from database
+router.route('/').get((req, res) => {
+    Exercise.find()
+        .then(exercises => res.json(exercises))
+        .catch(err => res.status(400).json("Error: " + err));
+});
+
+
+/** add new exercise */
+router.route('/add').post((req, res)=> {
+    // data
+    const username = req.body.username; 
+    const description = req.body.description; 
+    const duration = Number(req.body.duration); 
+    const date = Date.parse(req.body.date);
+
+    // new data
+    const newExercise = new Exercise({
+        username, 
+        description, 
+        duration, 
+        date
+    }); 
+
+    // save new data 
+    newExercise.save()
+        .then(()=>res.json("Exercise added successfully!"))
+        .catch(err => res.status(400).json('Error: ' + err))
+});
+
+/** show details */
+router.route('/:id').get((req, res)=> {
+    Exercise.findById(req.params.id)
+    .then(exercise => res.json(exercise))
+    .catch(err => res.status(400).json('Error: ' + err));
+})
+
+/** delete excercise */
+router.route('/:id').delete((req, res) => {
+    Exercise.findByIdAndDelete()
+        .then( () => res.json('Exercise deleted!'))
+        .catch( err => res.status(400).json('Error: ' + err )); 
+})
+
+/** udpate exercise */
+router.route('/update/:id').post( (req, res) => {
+    Exercise.findById(req.params.id)
+        .then( exercise => {
+            exercise.username = req.body.username; 
+            exercise.description = req.body.description; 
+            exercise.duration = Number(req.body.duration);
+            exercise.date = Date.parse(req.body.date); 
+
+            exercise.save()
+            .then( () => res.json('Exercise updated!'))
+            .catch( err => res.status(400).json('Error: ' + err ));
+        })
+        .catch(err => res.status(400).json('Error: ' + err));
+})
+
+
+/** export */
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,31 @@
+const express = require('express'); 
+const cors = require('cors');
+const mongoogse = require('mongoose');
+
+require('dotenv').config();
+
+const app = express(); 
+const port = process.env.PORT || 5000; 
+
+app.use(cors()); 
+app.use(express.json());
+const uri = process.env.ATLAS_URI; 
+
+mongoogse.connect(uri, { useNewUrlParser:true, useCreateIndex:true, useUnifiedTopology:true });
+const connection = mongoogse.connection;
+connection.once('open', ()=> {
+    console.log("MongoDB Database connection stublished suucessfully.");
+});
+
+
+// Routes
+const excercisesRouter = require('./routes/exercises.js');
+const usersRouter = require('./routes/users.js');
+app.use('/excercises', excercisesRouter);
+app.use('/users', usersRouter);
+
+
+//Listern
+app.listen(port, () => {
+    console.log(`Server starts is running port : ${port}`);
+})
